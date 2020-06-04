f381db3c94f2e8d7921ba89f72fbbfd9594f8d53
Hammad Haqqani <hhaqqani@MacBook-Pro.local>
2020-06-04 12:51:06 -0500

    adding network module

---
+++ Network/Securitygroup/variable.tf
@@ -0,0 +1,55 @@
+variable "availability_zone" {
+  description = "availability zone used for the demo, based on AWS region"
+  default = {
+    us-west-2 = "us-west-2c"
+  }
+}
+
+variable "hostname" {
+  description = "Host name of EC2 Machine"
+  default = "Dev-Test-Instance"
+}
+
+variable "role" {
+  description = "Role of the EC2 Machine"
+  default = "Database"
+}
+
+variable "xmcc" {
+  description = "Cost Center"
+  default = "9243"
+}
+
+variable "appname" {
+  description = "Application Name"
+  default = "ITOPS"
+}
+
+variable "dpstate" {
+  description = "DeploymentState"
+  default = "Dev"
+}
+
+
+variable "rdsname" {
+  description = "Name for the RDS Instance"
+}
+
+variable "username" {
+  description = "Username for RDS Instance"  
+}
+
+variable "password" {
+  description = "Password for RDS Instance"  
+}
+
+
+variable "instanceclass" {
+  description = "DB Instance Size"
+  default = "db.t2.large"  
+}
+
+variable "identifier" {
+  description = "Identifier name for instance"
+  default = "terraformtest"  
+}
